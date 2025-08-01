f52515d047654ac781340c113ea478ae521b624e
Nico Barberis <nico09barberis@gmail.com>
2025-08-01 10:24:35 -0300

    Proyecto listo para deploy

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Conectado a MongoDB");
+    app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+  })
+  .catch((err) => console.error("Error conectando a MongoDB:", err));
